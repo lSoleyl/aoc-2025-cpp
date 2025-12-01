62b0a68e0ec2ebcd758df1378988e239c396956a
lSoleyl <lSoleyl+github@gmail.com>
2025-12-01 19:57:35 +0100

    Day 1 Part 1 completed

--- 01/01.cpp
+++ 01/01.cpp
@@ -2,14 +2,41 @@
 #include <common/time.hpp>
 #include <common/task.hpp>
 
+std::vector<int> parseInstructions(std::istream&& input) {
+  std::vector<int> instructions;
 
+  while (input) {
+    char direction;
+    int distance;
+    input >> direction >> distance;
+
+    if (input) { // read successful
+      instructions.push_back(direction == 'R' ? distance : -distance);
+    }
+  }
+
+  return instructions;
+}
+
+const int DIAL_SIZE = 100;
 
 int main() {
   common::Time t;
 
-  auto input = task::input();
+  auto instructions = parseInstructions(task::input());
 
   int part1 = 0;
+
+  int dial = 50;
+  for (auto delta : instructions) {
+    dial = (dial + delta + DIAL_SIZE) % DIAL_SIZE;
+    if (dial == 0) {
+      ++part1;
+    }
+  }
+
+
+
   int part2 = 0;
 
   std::cout << "Part 1: " << part1 << "\n";
