7376b9cd6f3f985eb6b802e89e8a3ebaa794ae9b
lSoleyl <lSoleyl+github@gmail.com>
2025-12-01 20:31:29 +0100

    Day 1 Part 2 completed
     - Added data zip

--- 01/01.cpp
+++ 01/01.cpp
@@ -26,18 +26,35 @@ int main() {
   auto instructions = parseInstructions(task::input());
 
   int part1 = 0;
+  int part2 = 0;
 
   int dial = 50;
   for (auto delta : instructions) {
-    dial = (dial + delta + DIAL_SIZE) % DIAL_SIZE;
+    int distToNextZero = delta > 0 ? DIAL_SIZE - dial :
+                          dial > 0 ? -dial : -DIAL_SIZE;
+
+    // Part 2
+    if (std::abs(delta) >= std::abs(distToNextZero)) {
+      // We will pass 0 at least once during this rotation
+      delta -= distToNextZero;
+      ++part2;
+
+      // Now we might wrap around more than once (count each time)
+      part2 += std::abs(delta) / DIAL_SIZE;
+      delta %= DIAL_SIZE;
+
+      dial = (delta + DIAL_SIZE) % DIAL_SIZE;
+    } else {
+      // We still need to apply modulo, because at 0 - 18 we don't "pass" 0, but we still need to wrap around
+      dial = (dial + delta + DIAL_SIZE) % DIAL_SIZE;
+    }
+
+    // Part 1
     if (dial == 0) {
       ++part1;
     }
   }
-
-
-
-  int part2 = 0;
+  
 
   std::cout << "Part 1: " << part1 << "\n";
   std::cout << "Part 2: " << part2 << "\n";
