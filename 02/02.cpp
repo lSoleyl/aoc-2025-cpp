d9f519620e9006db164fc7693126116f4563cf64
lSoleyl <lSoleyl+github@gmail.com>
2025-12-02 20:35:36 +0100

    Day 2 Part 1 redesigned
    Solution is now much more preformant (possible candiates are checked
    directly) and part 2 should now be trivial.

--- 02/02.cpp
+++ 02/02.cpp
@@ -3,6 +3,37 @@
 #include <common/task.hpp>
 #include <common/regex.hpp>
 
+/** Caluclate power of 10 for huge numbers (without risk of double conversion errors)
+ */
+int64_t power10(int exponent) {
+  int64_t result = 1;
+  for (; exponent > 0; --exponent) {
+    result *= 10;
+  }
+  return result;
+}
+
+/** Divide by the same number multiple times
+ */
+int64_t divPower(int64_t number, int64_t divisor, int divisorExponent) {
+  for (; divisorExponent > 0; --divisorExponent) {
+    number /= divisor;
+  }
+  return number;
+}
+
+/** Repeat a prefix multiple times be repeated multiplication and addition 12 -> 12*10+12 = 1212 -> ...
+ */
+int64_t repeatNumber(int64_t prefix, int64_t factor, int times) {
+  int64_t result = 0;
+  for (; times > 0; --times) {
+    result *= factor;
+    result += prefix;
+  }
+  return result;
+}
+
+
 struct Range {
   int64_t begin;
   int64_t end;
@@ -12,6 +43,52 @@ struct Range {
   int64_t size() const {
     return end - begin;
   }
+
+  int64_t contains(int64_t number) const {
+    return number >= begin && number < end;
+  }
+
+  /** Calculates the invalid id sum for this range (Part 1)
+   *  The main idea is to determine the a prefix, which we will increment until we are out of range.
+   *  Then we can check the numbers directly by simply duplicating the prefix multiple times.
+   * 
+   *  This is a much more efficient an direct solution than searching for repetitions in all numbers within the
+   *  range, because we only check the possible candiates and don't check every number in the range.
+   */
+  int64_t invalidIdSum() const {
+    int64_t sum = 0;
+
+    auto beginDigits = static_cast<int>(std::log10(begin)) + 1;
+    auto lastDigits = static_cast<int>(std::log10(end-1)) + 1;
+    for (int digits = beginDigits; digits <= lastDigits; ++digits) {
+      // Ignore all odd digit numbers when checking for numbers with 2 parts
+      //for (int parts = digits; parts > 1; --parts) {
+      int parts = 2;
+        if (digits % parts == 0) {
+          // The divisor is used to extract the prefix from the number and continue incrementing it
+          auto divisor = static_cast<int>(power10(digits/parts));
+          // We can increment the prefix up to the divisor before we would increase the number of digits
+          auto prefixEnd = divisor;
+
+
+          auto beginNumber = std::max(begin, power10(digits-1));
+          for (auto prefix = divPower(beginNumber, divisor, parts-1); repeatNumber(prefix, divisor, parts) < end && prefix < prefixEnd; ++prefix) {
+            auto number = repeatNumber(prefix, divisor, parts);
+            // We the range may not contain the number if we have 288352 as begin, then we start checking for 288288, which is not in the range
+            if (contains(number)) {
+              sum += number;
+            }
+          }
+        }
+      //}
+    }
+
+    return sum;
+
+  }
+
+
+
 
   static std::vector<Range> parseRanges(std::string input) {
     std::vector<Range> ranges;
@@ -34,18 +111,7 @@ int main() {
   int part2 = 0;
 
   for (auto& range : ranges) {
-    for (auto number = range.begin; number != range.end; ++number) {
-      auto power = static_cast<int>(std::log10(number));
-      if (power % 2 == 1) {
-        power = ((power + 1) / 2);
-        // Odd powers of 10 correspond to even number of digits
-        auto divisor = static_cast<int64_t>(std::pow(10, power));
-        auto div = std::div(number, divisor);
-        if (div.quot == div.rem) {
-          part1 += number;
-        }
-      }
-    }
+    part1 += range.invalidIdSum();
   }
 
   std::cout << "Part 1: " << part1 << "\n";
