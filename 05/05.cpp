42b962d8e7d7d4908cdc3455a2fe53a9a03b4492
lSoleyl <lSoleyl+github@gmail.com>
2025-12-05 20:36:18 +0100

    Day 5 Part 2 completed
    Simplified and sped up part1 too while I was at it

--- 05/05.cpp
+++ 05/05.cpp
@@ -9,11 +9,24 @@
 
 using Id = int64_t;
 
-struct IdRange {
+struct Range {
   Id begin, end;
 
   bool contains(Id id) const {
     return id >= begin && id < end;
+  }
+
+  bool operator<(const Range& other) const {
+    return begin < other.begin;
+  }
+
+  void merge(const Range& other) {
+    begin = std::min(begin, other.begin);
+    end = std::max(end, other.end);
+  }
+
+  int64_t size() const {
+    return end - begin;
   }
 };
 
@@ -29,9 +42,39 @@ struct Ingredients {
     while (!(line = stream::line(input)).empty()) {
       available.push_back(string_view::into<Id>(line));
     }
+
+    reorganize();
   }
 
-  std::vector<IdRange> fresh;
+  void reorganize() {
+    // Sort ranges by begin position
+    std::sort(fresh.begin(), fresh.end());
+
+    // Now merge consecutive overlapping ranges
+    auto writePos = fresh.begin();
+    for (auto it = fresh.begin()+1, end = fresh.end(); it != end; ++it) {
+      if (writePos->contains(it->begin)) {
+        // We have overlap
+        writePos->merge(*it);
+      } else {
+        // No overlap, move write position by one and copy it there
+        *(++writePos) = *it;
+      }
+    }
+
+    // Now we can remove all elements between writePos and end as they have been removed by merging them
+    fresh.erase(writePos + 1, fresh.end());
+  }
+
+
+  bool isFresh(Id id) {
+    // Use binary search to faster find the matching range
+    // we search for the first end ordered AFTER the id to find the range which contains the id
+    auto pos = std::upper_bound(fresh.begin(), fresh.end(), id, [](Id id, const Range& range) { return id < range.end; });
+    return pos != fresh.end() && pos->contains(id);
+  }
+
+  std::vector<Range> fresh;
   std::vector<Id> available;
 };
 
@@ -46,13 +89,11 @@ int main() {
 
   Ingredients ingredients(task::input());
 
-  for (auto id : ingredients.available) {
-    if (std::ranges::any_of(ingredients.fresh, [=](const IdRange& range) { return range.contains(id); })) {
-      ++part1;
-    }
+  part1 = std::ranges::count_if(ingredients.available, [&](Id id) { return ingredients.isFresh(id); });
+  
+  for (auto& range : ingredients.fresh) {
+    part2 += range.size();
   }
-
-
 
 
   std::cout << "Part 1: " << part1 << "\n";
