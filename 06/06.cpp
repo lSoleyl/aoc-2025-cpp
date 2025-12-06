1827b0ab257a00a914de48d0dcb33dde9fd86fe5
lSoleyl <lSoleyl+github@gmail.com>
2025-12-06 16:09:45 +0100

    Rewrote Part 1 with a Column structure to simplify Part 2

--- 06/06.cpp
+++ 06/06.cpp
@@ -1,8 +1,10 @@
 #include <common/time.hpp>
 #include <common/task.hpp>
 #include <common/stream.hpp>
-#include <common/regex.hpp>
+#include <common/math.hpp>
+#include <common/string_view.hpp>
 
+#include <ranges>
 
 int64_t add(int64_t a, int64_t b) {
   return a + b;
@@ -12,64 +14,90 @@ int64_t multiply(int64_t a, int64_t b) {
   return a * b;
 }
 
+using OperatorFn = int64_t (*)(int64_t a, int64_t b);
+
+struct Column {
+  Column(char op) : width(1), operatorFn(op == '*' ? multiply : add) {}
+
+  int64_t simpleResult() const {
+    int64_t result = (operatorFn == add) ? 0 : 1; // initialize result to neutral element for the operation
+
+    for (auto& number : numbers) {
+      result = operatorFn(result, string_view::into<int64_t>(string_view::trim(number)));
+    }
+
+    return result;
+  }
+
+
+
+  std::vector<std::string> numbers; // The full with numbers of the column (optionally with leading and/or trailing spaces)
+  OperatorFn operatorFn;
+  int width; // the width of this column in digits/chars
+};
+
 
 struct Tasks {
-  Tasks(std::istream&& input) : columns(0), rows(0) {
+  Tasks(std::istream&& input) {
     // If we assume that we have the same amount of numbers in each row, we can just store all
     // numbers in one flat array and store the number of columns
-    std::regex numberRegex("[0-9]+");
-    std::regex operatorRegex("[+*]");
 
-    for (auto line : stream::lines(input)) {
-      if (line[0] != '*' && line[0] != '+') {
-        // Hande numbers
-        for (auto match : regex::iter(line, numberRegex)) {
-          numbers.push_back(std::stoi(match[0].str()));
-        }
-        if (!columns) {
-          // Set column size
-          columns = numbers.size();
-        }
-        ++rows;
-      } else {
-        // Handle operators
-        for (auto match : regex::iter(line, operatorRegex)) {
-          operators.push_back(*match[0].first);
-        }
+    // First collect all the lines in a vector
+    auto numberLines = stream::lines(input) | std::ranges::to<std::vector>();
+
+    // The last line will be the operator line, which will define the column width for each column,
+    // so we will process it first
+    columns = processOperatorLine(numberLines.back());
+    numberLines.pop_back();
+
+    // Read all the numbers as strings (including whitespaces) into the columns
+    for (auto& line : numberLines) {
+      size_t offset = 0;
+      for (auto& column : columns) {
+        column.numbers.push_back(line.substr(offset, column.width));
+        offset += column.width + 1;
       }
     }
   }
 
-  /** Access to a number in the field by row,column
+  std::pair<int64_t, int64_t> calculateResults() const {
+    int64_t totalSum = 0;   // Part 1
+    int64_t correctSum = 0; // Part 2
+    
+    for (auto& column : columns) {
+      totalSum += column.simpleResult();
+      
+    }
+
+    return { totalSum, correctSum };
+  }
+
+
+private:
+
+  /** This method will determine the number of column, the width of each column and
+   *  will store the operators to apply to each column
    */
-  int get(int row, int column) const {
-    return numbers[row * columns + column];
-  }
+  static std::vector<Column> processOperatorLine(const std::string& line) {
+    std::vector<Column> columns;
+    for (auto ch : line) {
+      if (ch == '*' || ch == '+') {
+        // Next operator
+        if (!columns.empty()) {
+          // Subtract 1 from previous column width for column separator
+          --columns.back().width; 
+        }
 
-
-  int64_t calculateResult() const {
-    int64_t totalSum = 0;
-    int column = 0;
-    for (auto op : operators) {
-      int64_t columnResult = (op == '*') ? 1 : 0; // initialize result with neutral element for the operation
-      auto opFn = (op == '*') ? multiply : add;
-
-      for (int row = 0; row < rows; ++row) {
-        columnResult = opFn(columnResult, get(row, column));
+        columns.push_back(ch);
+      } else {
+        // Otherwise this space is part of the previous column
+        ++columns.back().width;
       }
-
-      totalSum += columnResult;
-      ++column;
     }
-
-    return totalSum;
+    return columns;
   }
 
-
-  int columns;
-  int rows;
-  std::vector<int> numbers;
-  std::vector<char> operators;
+  std::vector<Column> columns;
 };
 
 
@@ -78,11 +106,8 @@ struct Tasks {
 int main() {
   common::Time t;
 
-  int64_t part1 = 0;
-  int64_t part2 = 0;
-
-  Tasks tasks(task::input());
-  part1 = tasks.calculateResult();
+  Tasks tasks(task::input("sample.txt"));
+  auto [part1,part2] = tasks.calculateResults();
 
   std::cout << "Part 1: " << part1 << "\n";
   std::cout << "Part 2: " << part2 << "\n";
