11e8961f96fd83ef09ac9643a78ac05ac5b97593
lSoleyl <lSoleyl+github@gmail.com>
2025-12-07 10:44:43 +0100

    Day 7 Part 2 completed
    Found a pretty efficient solution right away

--- 07/07.cpp
+++ 07/07.cpp
@@ -5,37 +5,72 @@
 #include <unordered_set>
 
 
-struct TachyonField : public Field {
-  TachyonField(std::istream&& input) : Field(std::move(input)) {}
+struct Tile {
+  Tile(char type) : type(type), paths(0) {}
+  char type;
+  int64_t paths; // the number of paths to take to the bottom from this field (Part 2)
+
+  bool operator==(const Tile& other) const { return type == other.type; }
+};
+
+
+struct TachyonField : public FieldT<Tile> {
+  TachyonField(std::istream&& input) : FieldT(std::move(input)) {}
 
   // Part 1
   int64_t countBeamSplits() {
-    std::unordered_set<Vector> visitedPositions;
     int64_t totalSplits = 0;
     auto startPos = fromOffset(findOffset('S'));
-    simulateBeam(startPos, visitedPositions, totalSplits);
+    simulateBeam(startPos, totalSplits);
 
     // Now count the resulting breams by 
     return totalSplits;
   }
 
+  // Part 2
+  int64_t countTimelines() {
+    // Simply running the regular beam simulation for all beam split possibilities would take too long, we need a more direct approach for this.
+    // Since we know all visited fields from our beam simulation, we will now check them bottom to top and count how many paths lead from that position
+    // to the top and store this number in the tile itself. Then we will be able to simply read out the number form the 'S' node.
+    std::vector<Vector> sortedPositions(visitedPositions.begin(), visitedPositions.end());
+    // Sort lower rows first, but also sort by ascending x values to read the row left to right (not really that important)
+    std::sort(sortedPositions.begin(), sortedPositions.end(), [](const Vector& a, const Vector& b) { return a.y > b.y || (a.y == b.y && a.x < b.x); });
+
+
+    for (auto position : sortedPositions) {
+      auto& tile = operator[](position);
+      if (tile.type == '^') {
+        // Splitter case: The number of paths equals the number of paths diagonally right and left below
+        // We don't take the right/left positions, because the right position hasn't been processed yet.
+        // From the input I can see that these positions will always be valid, so we can just access them
+        tile.paths = operator[](position + Vector::DownLeft).paths + operator[](position + Vector::DownRight).paths;
+      } else {
+        // Simple case: The number of paths is the same as the field right below
+        // In case there is no tile below, we have exactly one path
+        tile.paths = validPosition(position + Vector::Down) ? operator[](position + Vector::Down).paths : 1;
+      }
+    }
+
+    // Now simply return the number of paths from the start position
+    return data[findOffset('S')].paths;;
+  }
+
 
 private:
-  void simulateBeam(Vector pos, std::unordered_set<Vector>& visitedPositions, int64_t& totalSplits) {
+  void simulateBeam(Vector pos, int64_t& totalSplits) {
     // Simulate the beam until we either we reach the end or encounter a position already touched by another beam
-    for (; validPosition(pos) && visitedPositions.insert(pos).second; pos += Vector::Down) {
-      if ((*this)[pos] == '^') { 
+    for (; validPosition(pos) && visitedPositions.insert(pos).second ; pos += Vector::Down) {
+      if (operator[](pos).type == '^') {
         // Encountered a splitter -> split into two and terminate this function
         ++totalSplits;
-        simulateBeam(pos + Vector::Left, visitedPositions, totalSplits);
-        simulateBeam(pos + Vector::Right, visitedPositions, totalSplits);
+        simulateBeam(pos + Vector::Left, totalSplits);
+        simulateBeam(pos + Vector::Right, totalSplits);
         return;
       }
     }
   }
 
-
-
+  std::unordered_set<Vector> visitedPositions;
 };
 
 
@@ -51,6 +86,7 @@ int main() {
 
   TachyonField field(task::input());
   part1 = field.countBeamSplits();
+  part2 = field.countTimelines();
 
   std::cout << "Part 1: " << part1 << "\n";
   std::cout << "Part 2: " << part2 << "\n";
